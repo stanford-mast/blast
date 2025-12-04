efc1087b5bc59c84ff2c4f69dea3caa6bfe3f076
Ron Yifeng Wang <78146401+ronyw7@users.noreply.github.com>
2025-12-04 02:20:07 -0800

    Instruct planner to launch first of n tasks upon start (#36)

--- blastai/planner.py
+++ blastai/planner.py
@@ -202,7 +202,7 @@ Plan: search for list of top 8 biotech companies --> get list of CEOs from that 
             if parallelism.get("first_of_n", False):
                 num_copies = self.constraints.first_of_n_num_copies
                 guidance_parts.append(
-                    f'Execute launch_subtask(task="{task_description}", optional_initial_search_or_url={initial_url}, num_copies={num_copies}) '
+                    f'Execute immediately: launch_subtask(task="{task_description}", optional_initial_search_or_url={initial_url}, num_copies={num_copies}) '
                     "--> then get_first_subtask_result with the returned subtask IDs"
                     " Do not attempt to complete the task yourself - delegate it to subtasks. Unless polling for subtask results repeatedly fails, in which case you can attempt to complete the task yourself."
                 )
